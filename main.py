c1069a03c15a604f26c05a007da8c4c10fc67217
AntonioLopes15 <99749334+AntonioLopes15@users.noreply.github.com>
2022-03-10 19:03:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,6 @@
 import numpy as np
 import math
 
-S0 = [100,100,100,100,100,100,100,100,100,100]
-E0 = [0,0,0,0,0,0,0,0,0,0]
-PreSym = [0,0,0,1,0,0,0,0,0,0]
-Asym = [0,0,0,0,0,0,0,0,0,0]
-Mild =[0,0,0,0,0,0,0,0,0,0] 
-Seve =[0,0,0,0,0,0,0,0,0,0]
-Hosp =[0,0,0,0,0,0,0,0,0,0]
-ICU =[0,0,0,0,0,0,0,0,0,0]
-Recov =[0,0,0,0,0,0,0,0,0,0]
-Deceas = [0,0,0,0,0,0,0,0,0,0]
-
 Sfinal = [0,0,0,0,0,0,0,0,0,0]
 
 
@@ -66,205 +55,197 @@ Csym = ([[6.815,1.071,0.935,2.214,1.229,0.939,0.5025,0.20498,0.0837,0.01194],
 cnt = {}
 cntransicao = {}
 
-tS0 = tuple(S0)
-tE0 = tuple(E0)
-tPreSym = tuple(PreSym)
-tAsym = tuple(Asym)
-tMild = tuple(Mild)
-tSeve = tuple(Seve)
-tHosp = tuple(Hosp)
-tICU = tuple(ICU)
-tRecov = tuple(Recov)
-tDeceas = tuple (Deceas)
 
-t0 = tuple([tS0,tE0,tPreSym,tAsym,tMild,tSeve,tHosp,tICU,tRecov,tDeceas])
-cnt[t0]=1
 
-t=0
-for t in range (2):
-        pstar = [] 
-        E1 = []
-        IPreSym1 = []
-        IAsym1 = []
-        IMild1 = []
-        ISeve1 = []
-        IHosp1 = []
-        IICU1 = []
-        Rasym1 = []
-        Rmild1 = []
-        RHosp1 = []
-        RICU1 = []
-        DHosp1 = []
-        DICU1 = [] 
-        
-        S2 = [0,0,0,0,0,0,0,0,0,0]
-        E2 = [0,0,0,0,0,0,0,0,0,0]  
-        PreSym2 = [0,0,0,0,0,0,0,0,0,0]
-        Asym2 = [0,0,0,0,0,0,0,0,0,0]
-        Mild2 = [0,0,0,0,0,0,0,0,0,0]
-        Seve2 = [0,0,0,0,0,0,0,0,0,0]
-        Hosp2 = [0,0,0,0,0,0,0,0,0,0]
-        ICU2 = [0,0,0,0,0,0,0,0,0,0]
-        Recov2 = [0,0,0,0,0,0,0,0,0,0]
-        Deceas2 = [0,0,0,0,0,0,0,0,0,0]
-        for j in range (10):
-            assert (S0[j] >= 0)
-            A1 = 0
-            for i in range (10):
-                A = ((Casym[j][i])*(PreSym[i]+Asym[i]))+(Csym[j][i]*
-                                                         (Mild[i]+Seve[i]))
-                A1 += A
-            p_t = 1-(math.exp(-(h+t)*A1))
-            pstar += [p_t]
-            
-            
-            ###Inicio das binomiais
-            Enew = np.random.binomial(S0[j],pstar[j]) 
-            E1 += [Enew]
+tempo=0
+for k in range (2):
+        S0 = [100,100,100,100,100,100,100,100,100,100]
+        E0 = [0,0,0,0,0,0,0,0,0,0]
+        PreSym = [0,0,0,1,0,0,0,0,0,0]
+        Asym = [0,0,0,0,0,0,0,0,0,0]
+        Mild =[0,0,0,0,0,0,0,0,0,0] 
+        Seve =[0,0,0,0,0,0,0,0,0,0]
+        Hosp =[0,0,0,0,0,0,0,0,0,0]
+        ICU =[0,0,0,0,0,0,0,0,0,0]
+        Recov =[0,0,0,0,0,0,0,0,0,0]
+        Deceas = [0,0,0,0,0,0,0,0,0,0]
+        for t in range (2):
+                pstar = [] 
+                E1 = []
+                IPreSym1 = []
+                IAsym1 = []
+                IMild1 = []
+                ISeve1 = []
+                IHosp1 = []
+                IICU1 = []
+                Rasym1 = []
+                Rmild1 = []
+                RHosp1 = []
+                RICU1 = []
+                DHosp1 = []
+                DICU1 = [] 
+                
+                S2 = [0,0,0,0,0,0,0,0,0,0]
+                E2 = [0,0,0,0,0,0,0,0,0,0]  
+                PreSym2 = [0,0,0,0,0,0,0,0,0,0]
+                Asym2 = [0,0,0,0,0,0,0,0,0,0]
+                Mild2 = [0,0,0,0,0,0,0,0,0,0]
+                Seve2 = [0,0,0,0,0,0,0,0,0,0]
+                Hosp2 = [0,0,0,0,0,0,0,0,0,0]
+                ICU2 = [0,0,0,0,0,0,0,0,0,0]
+                Recov2 = [0,0,0,0,0,0,0,0,0,0]
+                Deceas2 = [0,0,0,0,0,0,0,0,0,0]
+                for j in range (10):
+                    assert (S0[j] >= 0)
+                    A1 = 0
+                    for i in range (10):
+                        A = ((Casym[j][i])*(PreSym[i]+Asym[i]))+(Csym[j][i]*
+                                                                 (Mild[i]+Seve[i]))
+                        A1 += A
+                    p_t = 1-(math.exp(-(h+t)*A1))
+                    pstar += [p_t]
+                    
+                    
+                    ###Inicio das binomiais
+                    Enew = np.random.binomial(S0[j],pstar[j]) 
+                    E1 += [Enew]
+                     
+                    IPreSymNew = np.random.binomial(E0[j],1-math.exp(-h*γ))
+                    IPreSym1 += [IPreSymNew]
+                    
+                    IAsymNew = np.random.binomial(PreSym[j],1-math.exp(-h*p[j]*σ))
+                    IAsym1 += [IAsymNew]
              
-            IPreSymNew = np.random.binomial(E0[j],1-math.exp(-h*γ))
-            IPreSym1 += [IPreSymNew]
-            
-            IAsymNew = np.random.binomial(PreSym[j],1-math.exp(-h*p[j]*σ))
-            IAsym1 += [IAsymNew]
-     
-            IMildNew = np.random.binomial(PreSym[j]-IAsym1[j],1-math.exp
-                                          (-h*(1-p[j]))*θ)
-            IMild1 += [IMildNew]
-            
-            ISeveNew = np.random.binomial(Mild[j],1-math.exp(-h*ψ[j]))
-            ISeve1 += [ISeveNew]
-            
-            IHospNew = np.random.binomial(Seve[j],1-math.exp(-h*ϕ1[j]*ω[j]))
-            IHosp1 += [IHospNew]
-            
-            IICUNew = np.random.binomial(Seve[j]-IHosp1[j],1-math.exp(-h*
-                                                            (1-ϕ1[j])*ω[j]))
-            IICU1 += [IICUNew]
-            
-            RasymNew = np.random.binomial(Asym[j], 1-math.exp(-h*δ1))
-            Rasym1 += [RasymNew]
-            
-            RmildNew = np.random.binomial(Mild[j]-ISeve1[j],1-math.exp(-h*δ2[j]))
-            Rmild1 += [RmildNew]
-            
-            RHospNew = np.random.binomial(Hosp[j],1-math.exp(-h*δ3[j]))
-            RHosp1 += [RHospNew]
-            
-            RICUNew = np.random.binomial(ICU[j],1-math.exp(-h*δ4[j]))
-            RICU1 += [RICUNew]
-            
-            DHospNew = np.random.binomial(Hosp[j]-RHosp1[j],1-math.exp(-h*τ1[j]))
-            DHosp1 += [DHospNew]
-            
-            DICUNew = np.random.binomial(ICU[j]-RICU1[j],1-math.exp(-h*τ2[j]))
-            DICU1 += [DICUNew]
-            
-            ###Inicio do passo seguinte 
-            S2New = []
-            S2New = S0[j]-E1[j]
-            S2[j] += S2New
-            
-            E2New = []
-            E2New = E0[j]+E1[j]-IPreSym1[j]
-            E2[j] += E2New
-            
-            IPreSym2New = []
-            IPreSym2New = PreSym[j]+IPreSym1[j]-IAsym1[j]-IMild1[j]
-            PreSym2[j] += IPreSym2New
-            
-            IAsym2New = []
-            IAsym2New = Asym[j]+IAsym1[j]-Rasym1[j]
-            Asym2[j] += IAsym2New
-            
-            IMild2New = []
-            IMild2New = Mild[j] + IMild1[j] - ISeve1[j] - Rmild1[j]
-            Mild2[j] += IMild2New
-            
-            ISeve2New = []
-            ISeve2New = Seve[j]+ISeve1[j]-IHosp1[j]-IICU1[j]
-            Seve2[j] = ISeve2New
-            
-            IHosp2New = []
-            IHosp2New = Hosp[j]+IHosp1[j]-DHosp1[j]-RHosp1[j]
-            Hosp2[j] = IHosp2New
-            
-            IICU2New = []
-            IICU2New = ICU[j]+IICU1[j]-DICU1[j]-RICU1[j]
-            ICU2[j] = IICU2New
-            
-            Recov2New = []
-            Recov2New = Recov[j]+Rasym1[j]+Rmild1[j]+RHosp1[j]+RICU1[j]
-            Recov2[j] = Recov2New
-            
-            Deceas2New = []
-            Deceas2New = Deceas[j] + DHosp1[j] + DICU1[j]
-            Deceas2[j] = Deceas2New   
-            
-        tS0 = tuple(S0)
-        tE0 = tuple(E0)
-        tPreSym = tuple(PreSym)
-        tAsym = tuple(Asym)
-        tMild = tuple(Mild)
-        tSeve = tuple(Seve)
-        tHosp = tuple(Hosp)
-        tICU = tuple(ICU)
-        tRecov = tuple(Recov)
-        tDeceas = tuple (Deceas)
+                    IMildNew = np.random.binomial(PreSym[j]-IAsym1[j],1-math.exp
+                                                  (-h*(1-p[j]))*θ)
+                    IMild1 += [IMildNew]
+                    
+                    ISeveNew = np.random.binomial(Mild[j],1-math.exp(-h*ψ[j]))
+                    ISeve1 += [ISeveNew]
+                    
+                    IHospNew = np.random.binomial(Seve[j],1-math.exp(-h*ϕ1[j]*ω[j]))
+                    IHosp1 += [IHospNew]
+                    
+                    IICUNew = np.random.binomial(Seve[j]-IHosp1[j],1-math.exp(-h*
+                                                                    (1-ϕ1[j])*ω[j]))
+                    IICU1 += [IICUNew]
+                    
+                    RasymNew = np.random.binomial(Asym[j], 1-math.exp(-h*δ1))
+                    Rasym1 += [RasymNew]
+                    
+                    RmildNew = np.random.binomial(Mild[j]-ISeve1[j],1-math.exp(-h*δ2[j]))
+                    Rmild1 += [RmildNew]
+                    
+                    RHospNew = np.random.binomial(Hosp[j],1-math.exp(-h*δ3[j]))
+                    RHosp1 += [RHospNew]
+                    
+                    RICUNew = np.random.binomial(ICU[j],1-math.exp(-h*δ4[j]))
+                    RICU1 += [RICUNew]
+                    
+                    DHospNew = np.random.binomial(Hosp[j]-RHosp1[j],1-math.exp(-h*τ1[j]))
+                    DHosp1 += [DHospNew]
+                    
+                    DICUNew = np.random.binomial(ICU[j]-RICU1[j],1-math.exp(-h*τ2[j]))
+                    DICU1 += [DICUNew]
+                    
+                    ###Inicio do passo seguinte 
+                    S2New = []
+                    S2New = S0[j]-E1[j]
+                    S2[j] += S2New
+                    
+                    E2New = []
+                    E2New = E0[j]+E1[j]-IPreSym1[j]
+                    E2[j] += E2New
+                    
+                    IPreSym2New = []
+                    IPreSym2New = PreSym[j]+IPreSym1[j]-IAsym1[j]-IMild1[j]
+                    PreSym2[j] += IPreSym2New
+                    
+                    IAsym2New = []
+                    IAsym2New = Asym[j]+IAsym1[j]-Rasym1[j]
+                    Asym2[j] += IAsym2New
+                    
+                    IMild2New = []
+                    IMild2New = Mild[j] + IMild1[j] - ISeve1[j] - Rmild1[j]
+                    Mild2[j] += IMild2New
+                    
+                    ISeve2New = []
+                    ISeve2New = Seve[j]+ISeve1[j]-IHosp1[j]-IICU1[j]
+                    Seve2[j] = ISeve2New
+                    
+                    IHosp2New = []
+                    IHosp2New = Hosp[j]+IHosp1[j]-DHosp1[j]-RHosp1[j]
+                    Hosp2[j] = IHosp2New
+                    
+                    IICU2New = []
+                    IICU2New = ICU[j]+IICU1[j]-DICU1[j]-RICU1[j]
+                    ICU2[j] = IICU2New
+                    
+                    Recov2New = []
+                    Recov2New = Recov[j]+Rasym1[j]+Rmild1[j]+RHosp1[j]+RICU1[j]
+                    Recov2[j] = Recov2New
+                    
+                    Deceas2New = []
+                    Deceas2New = Deceas[j] + DHosp1[j] + DICU1[j]
+                    Deceas2[j] = Deceas2New   
+                    
+                tS0 = tuple(S0)
+                tE0 = tuple(E0)
+                tPreSym = tuple(PreSym)
+                tAsym = tuple(Asym)
+                tMild = tuple(Mild)
+                tSeve = tuple(Seve)
+                tHosp = tuple(Hosp)
+                tICU = tuple(ICU)
+                tRecov = tuple(Recov)
+                tDeceas = tuple (Deceas)
+                
+                tS2 = tuple(S2)
+                tE2 = tuple(E2)
+                tPreSym2 = tuple(PreSym2)
+                tAsym2 = tuple(Asym2)
+                tMild2 = tuple(Mild2)
+                tSeve2 = tuple(Seve2)
+                tHosp2 = tuple(Hosp2)
+                tICU2 = tuple(ICU2)
+                tRecov2 = tuple(Recov2)
+                tDeceas2 = tuple (Deceas2)
         
-        tS2 = tuple(S2)
-        tE2 = tuple(E2)
-        tPreSym2 = tuple(PreSym2)
-        tAsym2 = tuple(Asym2)
-        tMild2 = tuple(Mild2)
-        tSeve2 = tuple(Seve2)
-        tHosp2 = tuple(Hosp2)
-        tICU2 = tuple(ICU2)
-        tRecov2 = tuple(Recov2)
-        tDeceas2 = tuple (Deceas2)
+                ttrans = tuple([tS0,tE0,tPreSym,tAsym,tMild,tSeve,tHosp,tICU,tRecov,tDeceas\
+                               ,tS2,tE2,tPreSym2,tAsym2,tMild2,tSeve2,tHosp2,tICU2,tRecov2\
+                                 ,tDeceas])
+                
+                t0 = tuple([tS0,tE0,tPreSym,tAsym,tMild,tSeve,tHosp,tICU,tRecov,tDeceas])
+        
+                
+                if ttrans in cntransicao:
+                    cntransicao[ttrans]+=1
+                
+                else:
+                    cntransicao[ttrans]=1
+                
+                                          
+                S0 = S2
+                E0 = E2
+                PreSym = PreSym2
+                Asym = Asym2
+                Mild = Mild2
+                Seve = Seve2
+                Hosp = Hosp2
+                ICU = ICU2
+                Recov = Recov2
+                Deceas = Deceas2 
+        
+                t0 = tuple([tS0,tE0,tPreSym,tAsym,tMild,tSeve,tHosp,tICU,tRecov,tDeceas])
+                
+                if t0 in cnt:
+                    cnt[t0]+=1
+                    
+                else:
+                    cnt[t0]=1
 
-        ttrans = tuple([tS0,tE0,tPreSym,tAsym,tMild,tSeve,tHosp,tICU,tRecov,tDeceas\
-                       ,tS2,tE2,tPreSym2,tAsym2,tMild2,tSeve2,tHosp2,tICU2,tRecov2\
-                         ,tDeceas])
-        
-        if ttrans in cntransicao:
-            cntransicao[ttrans]+=1
-        
-        else:
-            cntransicao[ttrans]=1
-        
-                                  
-        S0 = S2
-        E0 = E2
-        PreSym = PreSym2
-        Asym = Asym2
-        Mild = Mild2
-        Seve = Seve2
-        Hosp = Hosp2
-        ICU = ICU2
-        Recov = Recov2
-        Deceas = Deceas2 
-        
-        tS0 = tuple(S0)
-        tE0 = tuple(E0)
-        tPreSym = tuple(PreSym)
-        tAsym = tuple(Asym)
-        tMild = tuple(Mild)
-        tSeve = tuple(Seve)
-        tHosp = tuple(Hosp)
-        tICU = tuple(ICU)
-        tRecov = tuple(Recov)
-        tDeceas = tuple (Deceas)
-
-        t0 = tuple([tS0,tE0,tPreSym,tAsym,tMild,tSeve,tHosp,tICU,tRecov,tDeceas])
-        
-        if t0 in cnt:
-            cnt[t0]+=1
-            
-        else:
-            cnt[t0]=1
-
-
+        tempo+=1
+        print(tempo)
 """        
         print ("step",t+1)
         print ("Suscetiveis:",S0,"Expostos:",E0,"Pré-Sintomáticos",PreSym,\
